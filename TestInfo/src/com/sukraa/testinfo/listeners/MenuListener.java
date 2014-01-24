7e93e7eb2b0af186f5c40e362f35ad3a0172854f
daystar7008 <daystar7008@gmail.com>
2014-01-24 18:09:55 +0530

    Create MenuListener.java

---
+++ TestInfo/src/com/sukraa/testinfo/listeners/MenuListener.java
@@ -0,0 +1,10 @@
+package com.sukraa.testinfo.listeners;
+
+public interface MenuListener extends android.widget.Button.OnClickListener {
+
+	public void onDepartmentClick();
+	public void onTestClick();
+	public void onProfileClick();
+	public void onFavoriteClick();
+	
+}
